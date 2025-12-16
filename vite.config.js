fe2b6042135ed5ba35a5d05a2cb581b8feefcf27
test <test@xxx.test>
2025-12-16 10:27:02 +0700

    add github pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/github-ssdlc/',   // 👈 เพิ่มบรรทัดนี้
   plugins: [
     vue(),
     vueDevTools(),
